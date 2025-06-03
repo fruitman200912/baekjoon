297650619e72732f9fa6a3d7de94dc8ff8bcdabc
윤재준 <fruitman200912@gmail.com>
2025-06-04 00:38:38 +0900

    B2

--- baekjoon.py
+++ baekjoon.py
@@ -2,10 +2,8 @@ n = int(input())
 f = int(input())
 
 for i in range(100):
-  if f < 11 and f == 0:
-    continue
-  if n%f == 0:
+  n = n - (n%100) + i
+  if n != 0 and n%f == 0:
     break
-  f = f - (f%100) + i
 
-print("%02d"%(f%100))
+print("%02d"%(n%100))
