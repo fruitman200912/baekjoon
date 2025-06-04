9d479b061d8d701704cc6aa70b6047beed5bb236
윤재준 <fruitman200912@gmail.com>
2025-06-05 00:13:10 +0900

    za

--- baekjoon.py
+++ baekjoon.py
@@ -6,12 +6,13 @@ while 1:
   for i in n_list:
     if num % i != 0:
       break
-    else:
-      for i in range(2,num):
-        if num % i == 0 and not(i in n_list):
-          break
   else:
-    break
+    for i in range(2,num):
+      if num % i == 0 and not(i in n_list):
+        num += 1
+        break
+    else:
+      break
   num += 1
 
 print(num)
