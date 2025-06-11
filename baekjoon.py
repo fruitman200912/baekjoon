487adeadf7abc8be76fbaa2bc8c5f5900c79ee92
윤재준 <fruitman200912@gmail.com>
2025-06-11 22:15:59 +0900

    B1

--- baekjoon.py
+++ baekjoon.py
@@ -1,10 +1,10 @@
-n = input().split()
+n = list(map(int,input().split()))
 
 i = 2
-while(1):
+while 1:
   count = 0
   for j in n:
-    if int(j) % i ==0:
+    if i % j ==0:
       count += 1
   if count>=3:
     break
