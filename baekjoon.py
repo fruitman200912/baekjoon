7255c6ed5a7151be1d8b949459fce9f0de11d093
윤재준 <fruitman200912@gmail.com>
2025-06-17 21:38:30 +0900

    B2

--- baekjoon.py
+++ baekjoon.py
@@ -1,5 +1,13 @@
-x, y, w, h = map(int,input().split())
+b = []
 
-a = [x, y, w-x, h-y]
+for i in range(8):
+  b.append(input())
 
-print(min(a))
+n = 0
+
+for i in range(8):
+  for j in range(i%2,8,2):
+    if b[i][j] == 'F':
+      n+=1
+
+print(n)
